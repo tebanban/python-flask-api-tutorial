db21b05f214741f09ff52884a6d00011264aebbd
tebanban <estebancalf@gmail.com>
2021-03-27 04:57:11 +0000

    exc 7.1

--- src/app.py
+++ src/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 todos=[
       {"label": "My first task", "done": False},
-      {"label": "My first task", "done": False}
+      {"label": "My second task", "done": False}
       ]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
     json_text = jsonify(todos)
     return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
  
 
 # These two lines should always be at the end of your app.py file.
